4299fbab8e56a2a85654f3a8084ae0daecf7cbc5
vviswadeep <vviswadeep@gmail.com>
2022-08-18 00:26:09 +0530

    4 files modified, 13 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common'; 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FirstPageComponent } from './first-page/first-page.component'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { SecondPageComponent } from './second-page/second-page.component';
+import { RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule, 
     NgbModule, 
-    FormsModule],
+    FormsModule,
+    CommonModule,
+    NgbNavModule,
+    RouterModule.forRoot([
+      // {path: '', redirectTo: '/home', pathMatch: 'full'},
+      {path: 'home', component: FirstPageComponent},
+      {path: 'events', component: SecondPageComponent},
+      {path: 'registration', component: RegistrationComponent},
+      {path: 'contact', component: ContactComponent},
+      {path: 'login', component: LoginComponent}
+    ]),
+  ],
   declarations: [ 
     AppComponent, 
     HelloComponent, 
     FirstPageComponent, 
-    SecondPageComponent 
+    SecondPageComponent,
+    RegistrationComponent,
+    ContactComponent,
+    LoginComponent
   ],
   bootstrap:    [ 
     AppComponent 
