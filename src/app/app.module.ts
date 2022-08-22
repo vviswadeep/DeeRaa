8bf4e135b072b04c134a94acc931f7f72a890899
vviswadeep <vviswadeep@gmail.com>
2022-08-22 08:41:23 +0530

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   declarations: [ 
     AppComponent, 
+    NavbarComponent,
     HelloComponent, 
     FirstPageComponent, 
     SecondPageComponent,
